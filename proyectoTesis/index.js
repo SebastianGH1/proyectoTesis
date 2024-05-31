ec0817bf198f0b55d4f9d8805dfc44fcae1851b8
SebastianGH1 <137653851+SebastianGH1@users.noreply.github.com>
2024-05-31 15:01:09 -0600

    Add files via upload
    se agrega el login del proyecto

---
+++ proyectoTesis/index.js
@@ -0,0 +1,41 @@
+document.getElementById("botonIrRegistro").addEventListener('click', function () {
+  window.location.href = 'registro.html';
+})
+//funcion de inicio de secion 
+document.getElementById("botonIniciarSesion").addEventListener('click', function () {
+  var usuario = document.getElementById("campoUsuario").value;
+  var password = document.getElementById("campoPassword").value;
+  var tipo = document.getElementById("campoTipo").value;
+
+  ejecutarPHP(`servicios/login.php?usuario=${usuario}&password=${password}&tipo=${tipo}`,
+    function (data) {
+      if (data.resultado == "ok") {
+        window.location.href = 'home.html?id=' + data.id_usuario;
+      } else {
+        var alert = `
+                <div class="alert alert-danger" role="alert">
+                    Usuario/Contraseña incorrecto.
+                </div>`
+        document.getElementById("contenedorAlertas").innerHTML = alert
+      }
+    })
+
+});
+
+function ejecutarPHP(url, callback) {
+  console.log(url);
+  fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("error! Status:" + response.status);
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+      callback(data);
+    })
+    .catch(error => {
+      console.error('Error en la solicitud:', error);
+    });
+}
